32101353ac70b61b47f107277efcc2bace853cf2
woranop <woranop_000@hotmail.com>
2019-11-26 11:12:00 +0700

    update album service

--- src/features/album/services.js
+++ src/features/album/services.js
@@ -5,5 +5,22 @@ export function getNewReleases({ token, limit }) {
 }
 
 export function getAlbumById(id, { token }) {
-  return API.getAlbumById(id, { token })
+  return API.getAlbumById(id, { token }).then(response => {
+    response.title = response.name
+    response.subTitle = response.artists.map(artist => artist.name).join(',')
+    response.image = response.images[0].url
+    response.bottomLine = `${new Date(response.release_date).getFullYear()} • ${
+      response.total_tracks
+    } SONGS`
+    response.tracks.items = response.tracks.items.map(item => {
+      item.artistsName = item.artists.map(artist => artist.name).join(',')
+      item.previewUrl = item.preview_url
+      item.artist = item.artistsName
+      item.image = response.image
+      item.album = response.name
+      item.durationMs = item.duration_ms
+      return item
+    })
+    return response
+  })
 }
